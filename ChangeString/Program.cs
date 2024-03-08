a53b1c0e6ac08a5e62a7aadf644b1fd34473b1a1
Izadora Chiamulera <a22301008@alunos.ulht.pt>
2024-03-08 15:03:42 +0000

    Adicionar novo projeto

---
+++ ChangeString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write a sentence please:");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("chose a character:");
+            string input2 = Console.ReadLine();
+            char cara = char.Parse(input2);
+
+            string stringFinal = "";
+
+            foreach (char a in input1)
+            {
+                if (a == cara)
+                {
+                    stringFinal += "X";
+                }
+                else
+                {
+                    stringFinal += a;
+                }
+            }
+
+            Console.WriteLine(stringFinal);
+            
+        }
+    }
+}
+    
